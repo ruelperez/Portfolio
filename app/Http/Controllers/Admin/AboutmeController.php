9ef3adab6105644e6a80b96925b16e36ec1dbd3c
ruelperez <karuelflorperezz@gmail.com>
2025-08-08 15:20:08 +0800

    setting up

--- app/Http/Controllers/Admin/AboutmeController.php
+++ app/Http/Controllers/Admin/AboutmeController.php
@@ -33,8 +33,9 @@ class AboutmeController extends Controller
     {
         $validated = $request->validated();
 
-        if ($request->hasfile('image')) {
-            $get_new_file = $request->file('image')->store('images');
+        if ($request->hasFile('image')) {
+            // Store in storage/app/public/images
+            $get_new_file = $request->file('image')->store('images', 'public');
             $validated['profile_pic'] = $get_new_file;
         }
 
@@ -42,4 +43,5 @@ class AboutmeController extends Controller
 
         return to_route('admin.aboutme.index')->with('message', 'Data Updated');
     }
+
 }
